761d7aeb90f65435f2246effb8c8ba12e639de8a
Yasir Omer <yasiromer@Yasirs-MacBook-Air.local>
2026-06-06 20:19:02 +0300

    Remove Instagram footer icon

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,17 +12,6 @@ const socialLinks = [
         <path d="M14 8.6V6.9c0-.8.2-1.3 1.3-1.3H17V2.3c-.8-.1-1.6-.2-2.4-.2-2.5 0-4.2 1.5-4.2 4.3v2.2H7.5V12h2.9v9.9H14V12h2.8l.4-3.4H14Z" />
       </svg>
     )
-  },
-  {
-    label: "Instagram",
-    href: "https://www.instagram.com/dds_sudan/",
-    icon: (
-      <svg viewBox="0 0 24 24" className="h-4 w-4" fill="none" stroke="currentColor" strokeWidth="2" aria-hidden="true">
-        <rect width="16" height="16" x="4" y="4" rx="4" />
-        <circle cx="12" cy="12" r="3.2" />
-        <path d="M16.8 7.2h.01" strokeLinecap="round" />
-      </svg>
-    )
   }
 ];
 
